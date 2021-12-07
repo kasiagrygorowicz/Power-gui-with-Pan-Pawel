150e17290ae9ed255c9be6605a4d237dfaaf6de6
kasiagrygorowicz <kasiagrygorowicz@interia.eu>
2021-12-07 16:25:17 +0100

    daily tips widget

--- src/components/widget/dailyTipsWidget/DailyTipsWidget.js
+++ src/components/widget/dailyTipsWidget/DailyTipsWidget.js
@@ -4,15 +4,17 @@ import widgetClasses from "../../layout/widget/Widget.module.css";
 import classes from "./DailyTipsWidget.module.css"
 import { StyledDailyTipsWitget } from "./StyledDailyTipsWidget.styled";
 import Widget from "../../layout/widget/Widget";
+import {useTranslation} from "react-i18next";
 
 function DailyTipsWidget(){
+    const t = useTranslation()[0]
     var text="Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
 return(
     <StyledDailyTipsWitget>
         <Widget className={"widgetClasses.basicShape"}>
 
 <Card.Body>
-  <Card.Title className={widgetClasses.titleStyle}>Daily Tips</Card.Title>
+  <Card.Title className={widgetClasses.titleStyle}>{t("dailyTipsW")}</Card.Title>
   <div className={classes.mainStructure}>
         <Tip tip={text}/>
         <Tip tip={text}/>
